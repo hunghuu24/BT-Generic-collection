f686ea65bf980d12e42febb4a070d43421dfeed3
Nguyen Huu Hung <hunghuu24@gmail.com>
2026-03-18 21:04:44 +0700

    Bài 4. Đếm số chẵn

---
+++ Bai4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Console.Write("Nhap so phan tu: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("a[{0}]: ", i);
+                int x = int.Parse(Console.ReadLine());
+                list.Add(x);
+            }
+            foreach (int x in list)
+            {
+                if(x % 2 == 0)
+                    Console.Write("{0} ", x);
+            }    
+        }
+    }
+}
