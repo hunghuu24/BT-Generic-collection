100ef7c1a789f38607ebd664fb6063b599277eaa
Nguyen Huu Hung <hunghuu24@gmail.com>
2026-03-18 22:32:48 +0700

    Bài 16. Đếm số lần xuất hiện

---
+++ Bai16/Program.cs
@@ -0,0 +1,32 @@
+﻿//Nguyễn Hữu Hưng 2415053122322
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap chuoi: ");
+            string input = Console.ReadLine();
+
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+            foreach (char c in input)
+            {
+                if (charCount.ContainsKey(c))
+                    charCount[c]++;
+                else
+                    charCount[c] = 1;
+            }
+
+            Console.WriteLine("\nSo lan xuat hien cua moi ky tu:");
+            foreach (var pair in charCount)
+                Console.WriteLine($"{pair.Key} : {pair.Value}");
+        }
+    }
+}
