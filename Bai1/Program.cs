3d1a2f446ba57ffa13a2c5eb065bef9a4dda03eb
Nguyen Huu Hung <hunghuu24@gmail.com>
2026-03-18 20:56:55 +0700

    Bai 1: Tạo danh sách số nguyên

---
+++ Bai1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                list.Add(x);
+            }
+            Console.Write("5 so bat ki: ");
+            foreach (int i in list)
+                Console.Write("{0} ", i);
+        }
+    }
+}
