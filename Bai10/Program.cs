046c1fa3d1fb67f4719762bd8c82b5436cddd247
Nguyen Huu Hung <hunghuu24@gmail.com>
2026-03-18 22:26:42 +0700

    Bài 10. Tìm phần tử nhỏ nhất

---
+++ Bai10/Program.cs
@@ -0,0 +1,27 @@
+﻿//Nguyễn Hữu Hưng 2415053122322
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Console.Write("Nhap so phan tu: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("a[{0}]: ", i);
+                int x = int.Parse(Console.ReadLine());
+                list.Add(x);
+            }
+            int min = list.Min();
+            Console.WriteLine($"Phan tu nho nhat: {min}");
+        }
+    }
+}
