86457d7e7f4c9969574679ba6cb1c47c94688fa4
Nguyen Huu Hung <hunghuu24@gmail.com>
2026-03-18 21:02:38 +0700

    Bài 2. Tính tổng các phần tử

---
+++ Bai2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Console.Write("Nhap so phan tu: ");
+            int n = int.Parse(Console.ReadLine());
+            for(int i  = 0; i < n; i++)
+            {
+                Console.Write("a[{0}]: ", i);
+                int x = int.Parse(Console.ReadLine());
+                list.Add(x);
+            }
+            int s = 0;
+            foreach (int i in list)
+                s = s + i;
+            Console.Write("Tong cac phan tu: {0}", s);
+        }
+    }
+}
