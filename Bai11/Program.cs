486485bc03e1c53f39a7b35da00ba1d8f4792bf3
Nguyen Huu Hung <hunghuu24@gmail.com>
2026-03-18 22:28:19 +0700

    Bài 11. Lưu danh sách sinh viên

---
+++ Bai11/Program.cs
@@ -0,0 +1,33 @@
+﻿//Nguyễn Hữu Hưng 2415053122322
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai11
+{
+    class Student
+    {
+        public int Id;
+        public string Name;
+    }
+
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student { Id = 1, Name = "Nguyen Van A" },
+                new Student { Id = 2, Name = "Tran Thi B" },
+                new Student { Id = 3, Name = "Le Minh C" }
+            };
+
+            Console.WriteLine("Danh sách sinh viên:");
+            Console.WriteLine("--------------------------");
+            foreach (var sv in students)
+                Console.WriteLine($"ID: {sv.Id} | Tên: {sv.Name}");
+        }
+    }
+}
