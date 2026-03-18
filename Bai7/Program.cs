238775e4a6595ba2880f38d04a1430f6ddcc8fec
Nguyen Huu Hung <hunghuu24@gmail.com>
2026-03-18 21:07:24 +0700

    Bài 7. Sắp xếp danh sách

---
+++ Bai7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Console.Write("Nhap so phan tu: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("a[{0}]: ", i);
+                int x = int.Parse(Console.ReadLine());
+                list.Add(x);
+            }
+            list.Sort();
+            foreach (int x in list)
+                Console.Write("{0} ", x);
+        }
+    }
+}
