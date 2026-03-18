a9b10b4e130f4b6cb1042dafdde674c99065be12
Nguyen Huu Hung <hunghuu24@gmail.com>
2026-03-18 22:24:35 +0700

    Bài 8. Đảo ngược danh sách

---
+++ Bai8/Program.cs
@@ -0,0 +1,27 @@
+﻿//Nguyễn Hữu Hưng 2415053122322
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Console.Write("Nhap so phan tu: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("a[{0}]: ", i);
+                int x = int.Parse(Console.ReadLine());
+                list.Add(x);
+            }
+            for(int i = list.Count - 1; i >= 0; i--)
+                Console.Write("{0} ", list[i]);
+        }
+    }
+}
