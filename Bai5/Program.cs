f318d17c1b97c13e2b62304ab8e2b65851041364
Nguyen Huu Hung <hunghuu24@gmail.com>
2026-03-18 21:05:32 +0700

    Bài 5. Xóa phần tử khỏi List

---
+++ Bai5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> list = new List<String>();
+            Console.Write("Nhap so sinh vien: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0}: ", i);
+                String x = Console.ReadLine();
+                list.Add(x);
+            }
+            foreach (String x in list)
+                Console.WriteLine("{0} ", x);
+            Console.Write("Nhap ten can xoa: ");
+            String y = Console.ReadLine();
+            list.Remove(y);
+            foreach (String x in list)
+                Console.WriteLine("{0} ", x);
+        }
+    }
+}
