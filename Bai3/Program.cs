a58237bc11cd5462737ca3e73495b8f41ecdfca9
Nguyen Huu Hung <hunghuu24@gmail.com>
2026-03-18 21:03:51 +0700

    Bài 3. Tìm số lớn nhất

---
+++ Bai3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Console.Write("Nhap so phan tu: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("a[{0}]: ", i);
+                int x = int.Parse(Console.ReadLine());
+                list.Add(x);
+            }
+            Console.WriteLine("So lon nhat: {0}", list.Max());
+        }
+    }
+}
