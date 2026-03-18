c3386878b9a0783a6a3f1261e1fd07875ae439c5
Nguyen Huu Hung <hunghuu24@gmail.com>
2026-03-18 22:30:01 +0700

    Bài 13. Đếm số sinh viên

---
+++ Bai13/Program.cs
@@ -0,0 +1,28 @@
+﻿//Nguyễn Hữu Hưng 2415053122322
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai13
+{
+    class Student
+    {
+        public int Id;
+        public string Name;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student { Id = 1, Name = "Nguyen Van Anh" },
+                new Student { Id = 2, Name = "Tran Thi Be" },
+                new Student { Id = 3, Name = "Le Minh Ca" }
+            };
+            Console.WriteLine($"Tong so sinh vien: {students.Count}");
+        }
+    }
+}
