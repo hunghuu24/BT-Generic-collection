e899441cf4a044be4206353a50b7a8c01d783a4f
Nguyen Huu Hung <hunghuu24@gmail.com>
2026-03-18 21:06:34 +0700

    Bài 6. Kiểm tra phần tử tồn tại

---
+++ Bai6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> list = new List<String>();
+            Console.Write("Nhap so: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0}: ", i);
+                String x = Console.ReadLine();
+                list.Add(x);
+            }
+            foreach (String x in list)
+                Console.WriteLine("{0} ", x);
+            Console.Write("Nhap chuoi can tim: ");
+            String y = Console.ReadLine();
+            int s = 0;
+            foreach (String x in list)
+                if (x == y)
+                    s = 1;
+            if (s == 1)
+                Console.WriteLine("Co ton tai");
+            else
+                Console.WriteLine("Khong ton tai");
+        }
+    }
+}
